1d3a3baf7700fd7651c49378ea9d4769bd34a21c
Abdelfattah Mohamed <abdelftahmohamed30@gmail.com>
2020-03-31 03:02:58 +0200

    240 ms

--- solutions/15. 3Sum.cpp
+++ solutions/15. 3Sum.cpp
@@ -6,9 +6,11 @@ public:
         unordered_map<int, int> mp;
         for(int i = 0; i < n; i++) mp[nums[i]] = i;
         vector<vector<int>> vec;
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n - 2; i++) {
+            if(nums[i] + nums[i + 1] + nums[i + 2] > 0) return vec;
             if(i > 0 && nums[i] == nums[i - 1]) continue;
-            for(int j = i + 1; j < n; j++) {
+            for(int j = i + 1; j < n - 1; j++) {
+                if(nums[i] + nums[j] + nums[j + 1] > 0) break;
                 if(nums[j] == nums[j - 1] && j != i + 1) continue;
                 int x = 0 - nums[i] - nums[j];
                 if(mp.count(x) && mp[x] > i && mp[x] > j) {
