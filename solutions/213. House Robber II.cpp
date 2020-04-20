827ad1d155a18e7cf941e3c1376a8eff1a9bbaae
Abdelfattah Mohamed <abdelftahmohamed30@gmail.com>
2020-04-20 09:44:25 +0200

    0 ms

--- solutions/213. House Robber II.cpp
+++ solutions/213. House Robber II.cpp
@@ -4,20 +4,25 @@ public:
         int n = nums.size();
         if(!n) return 0;
         if(n == 1) return nums[0];
-        if(n == 2) return max(nums[0], nums[1]);
-        vector<int> x = nums;
-        nums[1] = max(nums[0], nums[1]);
-        x[2] = max(x[1], x[2]);
-        for(int i = 2; i < n - 1; i++) nums[i] = max(nums[i - 2] + nums[i], nums[i - 1]);
-        for(int i = 3; i < n; i++) x[i] = max(x[i - 2] + x[i], x[i - 1]);
-        return max(nums[n - 2], x[n - 1]);
+        return max(solver(nums, 0, n - 1), solver(nums, 1, n));
     }
+    
+    int solver(vector<int> &nums, int l, int r) {
+        int prev = 0, curr = 0;
+        while(l < r) {
+            int temp = max(curr, prev + nums[l++]);
+            prev = curr, curr = temp;
+        }
+        return curr;
+    }
+    
+    
 };
 ​
 ​
 /*
 ​
     Time Complexity  => O(n)
-    Space Complexity => O(n)
+    Space Complexity => O(1)
 ​
 */
